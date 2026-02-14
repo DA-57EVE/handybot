ad3f154e3f2ce9bfe70fb28648a197c5ae81b82b
SteveN <steve@rholbenroad.uk>
2026-02-14 21:07:11 +0000

    Structure

---
+++ src/client/java/handybot/HAndyBotClient.java
@@ -0,0 +1,10 @@
+package handybot;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HAndyBotClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/handybot/HAndyBotDataGenerator.java
@@ -0,0 +1,11 @@
+package handybot;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HAndyBotDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
